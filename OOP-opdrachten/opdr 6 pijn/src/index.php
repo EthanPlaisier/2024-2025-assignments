d23b3d291042731c283e4156557385954055e92e
Ethan <9018008@student.zadkine.nl>
2025-03-20 10:34:19 +0100

    DIT WERKT NIET

---
+++ OOP-opdrachten/opdr 6 pijn/src/index.php
@@ -0,0 +1,62 @@
+<?php
+    // Functie: programma login OOP 
+    // Auteur: Ethan
+
+    // Initialisatie
+?>
+
+<!DOCTYPE html>
+
+<html lang="en">
+
+<body>
+
+	<h3>PDO Login and Registration</h3>
+	<hr/>
+
+	<h3>Welcome op de HOME-pagina!</h3>
+	<br />
+	<?php
+
+	require_once "../vendor/autoload.php";
+	use Login\classes\User;
+
+	//include 'classes/user.php'; 
+	//include 'config/database.php'; 
+	$database = new Database();
+	$db = $database->getConnection();
+
+    $user = new User($db);
+
+	// Activeer de session
+	//session_start();
+
+	// Indien Logout geklikt
+	if (isset($_GET['logout']) && $_GET['logout'] == 'true') {
+		$user->Logout();
+	}
+
+	// Check login session: staat de user in de session?
+	if(!$user->IsLoggedin()){
+		// Alert not login
+		echo "U bent niet ingelogd. Login in om verder te gaan.<br><br>";
+		// Toon login button
+		echo '<a href = "login_form.php">Login</a>';
+	} else {
+		
+		// select userdata from database
+		$user->GetUser();
+
+		
+		// Print userdata
+		echo "<h2>Het spel kan beginnen</h2>";
+		echo "Je bent ingelogd met:<br/>";
+		$user->ShowUser();
+		echo "<br><br>";
+		echo '<a href = "?logout=true">Logout</a>';
+	}
+	
+	?>
+
+</body>
+</html>
---
+++ OOP-opdrachten/opdr 6 pijn/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'ethan/login',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'c400a724ec49f8c541cf120b26cf57521497d718',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'ethan/login' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'c400a724ec49f8c541cf120b26cf57521497d718',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
